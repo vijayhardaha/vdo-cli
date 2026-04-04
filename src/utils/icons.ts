a26a6a2640ed2ddf7cec1376667679db87affbc0
Vijay Hardaha <vijayhardaha@proton.me>
2026-04-05 04:19:03 +0530

    feat: add yoctocolors icons for output

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,5 +1,5 @@
 import isUnicodeSupported from 'is-unicode-supported';
-import { blue, green, yellow, red, cyan, gray } from 'yoctocolors';
+import { blue, green, yellow, red, white } from 'yoctocolors';
 
 const _isUnicodeSupported = isUnicodeSupported();
 
@@ -7,5 +7,4 @@ export const info = blue(_isUnicodeSupported ? 'ℹ' : 'i');
 export const success = green(_isUnicodeSupported ? '✔' : '√');
 export const warning = yellow(_isUnicodeSupported ? '⚠' : '‼');
 export const error = red(_isUnicodeSupported ? '✖' : '×');
-export const loading = gray(_isUnicodeSupported ? '' : '');
-export const text = cyan(_isUnicodeSupported ? '' : '');
+export const loading = white(_isUnicodeSupported ? '' : '');
